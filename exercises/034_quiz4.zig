ac7c665f9cd0899166dd07fcac2920edabe78639
alexpresthus <alexander.presthus@gmail.com>
2024-05-08 21:05:59 +0200

    Fixed 001 - 044

---
+++ exercises/034_quiz4.zig
@@ -0,0 +1,29 @@
+//
+// Quiz time. See if you can make this program work!
+//
+// Solve this any way you like, just be sure the output is:
+//
+//     my_num=42
+//
+const std = @import("std");
+
+const NumError = error{IllegalNumber};
+
+pub fn main() void {
+    const stdout = std.io.getStdOut().writer();
+
+    const my_num = getNumber();
+    if (my_num) |value| {
+        stdout.print("my_num={}\n", .{value}) catch |e| {
+            std.debug.print("error={}", .{e});
+        };
+    } else |err| {
+        std.debug.print("error={}", .{err});
+    }
+}
+
+// This function is obviously weird and non-functional. But you will not be changing it for this quiz.
+fn getNumber() NumError!u32 {
+    if (false) return NumError.IllegalNumber;
+    return 42;
+}
